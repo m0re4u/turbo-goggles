10870c5d6c02078d98e7bea23a41dfff47d8598e
Michiel van der Meer <michielmeer@live.nl>
2019-04-15 15:07:00 +0200

    Segmenter for segment and words

--- babyai_treegen/level_gen.py
+++ babyai_treegen/level_gen.py
@@ -4,36 +4,33 @@ import gym
 
 import babyai
 
-from segment import Segment
+from segment import Segmenter
 
 def main(args):
     env = gym.make(args.env)
     c = Counter()
+    segger = Segmenter(args.segment_level)
+
     for _ in range(args.n_missions):
         obs = env.reset()
         sent = obs['mission']
         print(f"Mission: {sent}")
         sent_list = sent.split()
-        cmd, obj1, loc, obj2 = segment_mission(sent)
-        c.update([cmd, obj1, loc, obj2])
-    print(c)
+        segs = segger.segment(sent)
+        c.update(segs)
 
-
-def segment_mission(instruction):
-    instruction_list = instruction.split()
-    cmd = instruction_list[:1]
-    obj1 = instruction_list[1:4]
-    loc = instruction_list[4:6]
-    obj2 = instruction_list[6:]
-    return Segment(cmd), Segment(obj1), Segment(loc), Segment(obj2)
-
-
+    print(f"{len(c)} unique segments")
+    for k, v in c.most_common(len(c)):
+        print(f"{k:80} : {v/args.n_missions}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--env", required=True,
                         help="name of the environment to be run (REQUIRED)")
+    parser.add_argument("--segment_level", type=str, default='segment',
+                        choices=['word', 'segment'],
+                        help="segment on word or clause level")
     parser.add_argument("--n_missions", type=int, default=100,
-                        help="name of the environment to be run (REQUIRED)")
+                        help="how many missions to sample")
     args = parser.parse_args()
     main(args)
--- babyai_treegen/segment.py
+++ babyai_treegen/segment.py
@@ -2,34 +2,40 @@ from enum import Enum
 class SegmentType(Enum):
     COMMAND = 1
     OBJECT = 2
-    PUTSPEC = 3
+    PUTOBJECT = 3
+    VERB = 4
+    NOUN = 5
+    PROP = 6
+    ARTICLE = 7
 
-commands = [
-    'put',
-    'go',
-    'pick',
-    'open'
-]
-
-objects = [
-    'ball',
-    'key',
-    'box'
-]
+words_to_wordtype = {
+    'put': SegmentType.VERB,
+    'go':  SegmentType.VERB,
+    'pick': SegmentType.VERB,
+    'open': SegmentType.VERB,
+    'ball': SegmentType.NOUN,
+    'key':  SegmentType.NOUN,
+    'box':  SegmentType.NOUN,
+    'door':  SegmentType.NOUN,
+    'red':  SegmentType.NOUN,
+    'green':  SegmentType.NOUN,
+    'blue':   SegmentType.NOUN,
+    'purple': SegmentType.NOUN,
+    'yellow': SegmentType.NOUN,
+    'grey':   SegmentType.NOUN,
+    'next': SegmentType.PROP,
+    'to':   SegmentType.PROP,
+    'a': SegmentType.ARTICLE,
+    'the': SegmentType.ARTICLE
+}
 
 class Segment():
-    def __init__(self, words):
+    def __init__(self, words, type):
         self.words = words
-        for word in words:
-            if word in commands:
-                self.type = SegmentType.COMMAND
-                break
-            elif word in objects:
-                self.type = SegmentType.OBJECT
-            else:
-                self.type = SegmentType.PUTSPEC
+        self.type = type
+
     def __repr__(self):
-        return f"{self.type} - {self.words}"
+        return f"{self.type:22} - {self.words}"
 
     def __hash__(self):
         return hash(str(self.words))
@@ -38,3 +44,26 @@ class Segment():
         _words = self.words == other.words
         _type = self.type == other.type
         return _words and _type
+
+    def __format__(self, fmt):
+        return format(str(self), fmt)
+
+class Segmenter():
+    def __init__(self, segment_level):
+        self.level = segment_level
+
+    def segment(self, instruction):
+        instruction_list = instruction.split()
+        if self.level == 'word':
+            return [Segment(x, words_to_wordtype[x]) for x in instruction_list]
+        elif self.level == 'segment':
+            # Mission specific segmentation? (hardcore??)
+            # This one is for PutNextLocal-v0
+            cmd = instruction_list[:1]
+            obj1 = instruction_list[1:4]
+            locobj = instruction_list[4:]
+            return [
+                Segment(cmd, SegmentType.COMMAND),
+                Segment(obj1, SegmentType.OBJECT),
+                Segment(locobj, SegmentType.PUTOBJECT)
+            ]
