1167dd27ca239d61512149d21e5fb7a1e773312f
Michiel van der Meer <michiel@vandermeer.ai>
2019-06-24 15:26:39 +0200

    Updated diagnostic training

--- train_diagnostic/train_diag.py
+++ train_diagnostic/train_diag.py
@@ -36,8 +36,10 @@ class DiagnosticDataset(Dataset):
 
 
 def validation_eval(val_set, model, do_entropy=False, limit=-1):
-    B_SIZE = 32
-    val_loader = DataLoader(val_set, batch_size=B_SIZE,
+    """
+    Perform evaluation on :val_set for :limit batches with :model
+    """
+    val_loader = DataLoader(val_set, batch_size=BATCH_SIZE,
                             shuffle=True, num_workers=0)
     correct = 0
     ents = []
@@ -54,14 +56,22 @@ def validation_eval(val_set, model, do_entropy=False, limit=-1):
         local_acc = sum(label.flatten() == output)
         correct += local_acc.item()
     if limit > 0:
-        div = limit * B_SIZE
+        div = limit * BATCH_SIZE
     else:
         div = len(val_set)
-    return correct / div, np.average(ents)
+
+    if do_entropy:
+        return correct / div, np.average(ents)
+    else:
+        return correct / div, np.nan
 
 
-def stop_on_accs(n, validation_accuracy, repeat_stop=5):
-    if (n // 300) > repeat_stop:
+def stop_on_accs(n, validation_accuracy, repeat_stop=5, print_every=300):
+    """
+    Check whether we had the same validation accuracy for :repeat_stop prints,
+    return a signal we should stop if that is the case.
+    """
+    if (n // print_every) > repeat_stop:
         accs = np.array(validation_accuracy[-repeat_stop:])
         red = accs - accs[-1]
         trth = np.isclose(red, np.zeros(red.shape))
@@ -75,10 +85,10 @@ def main(args):
     split = int(np.floor(0.2 * len(dataset)))
     train_set, val_set = torch.utils.data.random_split(
         dataset, [len(dataset) - split, split])
-    train_loader = DataLoader(train_set, batch_size=32,
+    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE,
                               shuffle=True, num_workers=0)
 
-    logging.info(f"Initialized dataset")
+    logging.info(f"Initialized dataset: {len(train_loader)} batches")
 
     model = torch.nn.Sequential(
         torch.nn.Linear(128, 18),
@@ -97,7 +107,7 @@ def main(args):
             break
         logging.info(f"Start of epoch: {i}")
         for n, batch in enumerate(train_loader):
-            t+=1
+            t += 1
             data_in = batch[0].to(device)
             label = batch[1].type(torch.long).to(device)
             optimizer.zero_grad()
@@ -105,7 +115,7 @@ def main(args):
             loss = criterion(output, label.flatten())
             loss.backward()
             optimizer.step()
-            if n % 300 == 0:
+            if n % args.print_every == 0:
                 val_acc, val_ent = validation_eval(
                     val_set, model, args.show_entropy)
                 train_acc, train_ent = validation_eval(
@@ -119,7 +129,7 @@ def main(args):
                     logging.info(
                         f"Epoch: {i:2} - Step {n:5} - Loss: {loss:1.3f} - TAcc: {train_acc:1.5f} - VAcc: {val_acc:1.5f}")
 
-                if stop_on_accs(t, val_accs, args.repeat_stop):
+                if stop_on_accs(t, val_accs, args.repeat_stop, args.print_every):
                     logging.warning("Stopping after stuck validation accuracy")
                     stop = True
                     break
@@ -130,7 +140,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--data_dir", default="reason_dataset/", type=str,
+    parser.add_argument("--data_dir", default="data/reason_dataset/", type=str,
                         help="Directory pointing to training data (REQUIRED)")
     parser.add_argument("--epochs", default=50, type=int,
                         help="How many epochs to train")
@@ -142,10 +152,13 @@ if __name__ == "__main__":
                         help="Seeding")
     parser.add_argument("--repeat_stop", default=10, type=int,
                         help="How many steps to check for validation accuracy stuck")
+    parser.add_argument("--print_every", default=100, type=int,
+                        help="Print every X batches")
     args = parser.parse_args()
 
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
 
     LOG_FORMAT = '%(asctime)s %(name)-6s %(levelname)-6s %(message)s'
     logging.basicConfig(format=LOG_FORMAT,
@@ -154,7 +167,6 @@ if __name__ == "__main__":
     config = vars(args)
     logging.info("Parameters:")
     for k, v in config.items():
-      logging.info(f"  {k:>21} : {v}")
-
+        logging.info(f"  {k:>21} : {v}")
 
     main(args)
