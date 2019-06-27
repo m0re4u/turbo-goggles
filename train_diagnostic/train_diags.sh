4667fa34655e564972090d86e4b8edd5bc0e1fd7
Michiel van der Meer <michiel@vandermeer.ai>
2019-06-27 15:47:37 +0200

    Script for training diagnostic classifiers

--- train_diagnostic/train_diags.sh
+++ train_diagnostic/train_diags.sh
@@ -1,30 +1,25 @@
 #! /bin/bash
 set -eu
 
-function train_diag {
-    # $1 jobid
-    # $2 checkno
-    # $3 LevelName
-    python3 ../../machine/eval_rl.py \
-    --env $3 \
-    --model ../../machine/models/$(ls $MODEL_DIR | grep $1)/$2_check.pt \
-    --vocab ../../machine/models/$(ls $MODEL_DIR | grep $1)/vocab.json \
-    --reasoning model \
-    --episodes $EPISODES;
 
+DATA_DIR=data/
+EPOCHS=10
+LR=0.001
+
+function train_diag {
+    # $1 seed
+    # $2 data_dir
+    # $3 outfile
+    python3 train_diag.py --seed $1 --lr $LR --epochs $EPOCHS --outfile $3 --data_dir $2
 }
 
-run_datagather 2603358 005600 BabyAI-CustomGoToObjSmall-v0
-cp -r data/reason_dataset data/small_new_seed1
-run_datagather 2604666 005900 BabyAI-CustomGoToObjAndOr-v0
-cp -r data/reason_dataset data/andor_new_seed1
-run_datagather 2604667 005400 BabyAI-CustomGoToObjMedium-v0
-cp -r data/reason_dataset data/beforeafter_new_seed1
-run_datagather 2300034 007600 BabyAI-CustomGoToObjSmall-v0
-cp -r data/reason_dataset data/small_base_seed1
-run_datagather 2489287 007600 BabyAI-CustomGoToObjSmall-v0
-cp -r data/reason_dataset data/small_base_seed42
-run_datagather 2604660 003000 BabyAI-CustomGoToObjMedium-v0
-cp -r data/reason_dataset data/beforeafter_base_seed1
-run_datagather 2604665 002300 BabyAI-CustomGoToObjAndOr-v0
-cp -r data/reason_dataset data/andor_base_seed1
+for dataset in $DATA_DIR/*; do
+    # train with seed 1
+    train_diag 1 $dataset/ "$(basename $dataset)"_diagseed1.pt
+
+    # train with seed 2
+    train_diag 2 $dataset/ "$(basename $dataset)"_diagseed2.pt
+
+    # train with seed 3
+    train_diag 3 $dataset/ "$(basename $dataset)"_diagseed3.pt
+done
