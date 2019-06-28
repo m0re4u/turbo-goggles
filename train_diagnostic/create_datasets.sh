9a87bc526d0601ff970e0903653879f0eef17ab8
Michiel van der Meer <michiel@vandermeer.ai>
2019-06-28 14:46:13 +0200

    More dataset creation

--- train_diagnostic/create_datasets.sh
+++ train_diagnostic/create_datasets.sh
@@ -8,26 +8,35 @@ function run_datagather {
     # $1 jobid
     # $2 checkno
     # $3 LevelName
+    # $4 Dataset dir
     python3 ../../machine/eval_rl.py \
     --env $3 \
     --model ../../machine/models/$(ls $MODEL_DIR | grep $1)/$2_check.pt \
     --vocab ../../machine/models/$(ls $MODEL_DIR | grep $1)/vocab.json \
     --reasoning model \
-    --episodes $EPISODES;
-
+    --gather \
+    --episodes $EPISODES > output.log
+    ACC=$(cat output.log | grep Accuracy)
+    echo "$1_$2_$3 $4  | $ACC " >> final_results_$EPISODES.log
+    cp -r data/reason_dataset data/$4
 }
 
-run_datagather 2603358 005600 BabyAI-CustomGoToObjSmall-v0
-cp -r data/reason_dataset data/small_new_seed1
-run_datagather 2604666 005900 BabyAI-CustomGoToObjAndOr-v0
-cp -r data/reason_dataset data/andor_new_seed1
-run_datagather 2604667 005400 BabyAI-CustomGoToObjMedium-v0
-cp -r data/reason_dataset data/beforeafter_new_seed1
-run_datagather 2300034 007600 BabyAI-CustomGoToObjSmall-v0
-cp -r data/reason_dataset data/small_base_seed1
-run_datagather 2489287 007600 BabyAI-CustomGoToObjSmall-v0
-cp -r data/reason_dataset data/small_base_seed42
-run_datagather 2604660 003000 BabyAI-CustomGoToObjMedium-v0
-cp -r data/reason_dataset data/beforeafter_base_seed1
-run_datagather 2604665 002300 BabyAI-CustomGoToObjAndOr-v0
-cp -r data/reason_dataset data/andor_base_seed1
+rm -f final_results_$EPISODES.log
+touch final_results_$EPISODES.log
+
+run_datagather 2603358 005600 BabyAI-CustomGoToObjSmall-v0 small_new_seed1
+run_datagather 2604666 005900 BabyAI-CustomGoToObjAndOr-v0 andor_new_seed1
+run_datagather 2607672 005400 BabyAI-CustomGoToObjAndOr-v0 andor_new_seed42
+run_datagather 2607673 006200 BabyAI-CustomGoToObjAndOr-v0 andor_new_seed100
+run_datagather 2604667 005400 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed1
+run_datagather 2607675 005600 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed42
+run_datagather 2607674 006500 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed100
+run_datagather 2300034 007600 BabyAI-CustomGoToObjSmall-v0 small_base_seed1
+run_datagather 2489287 007600 BabyAI-CustomGoToObjSmall-v0 small_base_seed42
+run_datagather 2607643 007100 BabyAI-CustomGoToObjSmall-v0 small_base_seed100
+run_datagather 2604660 003000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed1
+run_datagather 2607640 007000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed42
+run_datagather 2607638 007100 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed100
+run_datagather 2604665 002300 BabyAI-CustomGoToObjAndOr-v0 andor_base_seed1
+run_datagather 2607635 006900 BabyAI-CustomGoToObjAndOr-v0 andor_base_seed42
+run_datagather 2607636 006800 BabyAI-CustomGoToObjAndOr-v0 andor_base_seed100
