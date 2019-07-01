88e4d5c441f453936f4a3eb6fd7d1cc3a4666ff0
Michiel van der Meer <michiel@vandermeer.ai>
2019-07-01 12:53:05 +0200

    Eval all trained diags online

--- train_diagnostic/eval_diag.sh
+++ train_diagnostic/eval_diag.sh
@@ -10,20 +10,40 @@ function eval_diag_online {
     # $2 checkno
     # $3 LevelName
     # $4 diag model name
-    python3 ../../machine/eval_rl.py \
-    --env $3 \
-    --model ../../machine/models/$(ls $BABY_MODEL_DIR | grep $1)/$2_check.pt \
-    --vocab ../../machine/models/$(ls $BABY_MODEL_DIR | grep $1)/vocab.json \
-    --reasoning diagnostic \
-    --diag_model $DIAG_MODEL_DIR/$4
-    --episodes $EPISODES > output.log
-    # ACC=$(cat output.log | grep Accuracy)
-    # echo "$1_$2_$3 $4  | $ACC " >> final_results_$EPISODES.log
-    # cp -r data/reason_dataset data/$4
+    for i in $(seq 1 3);
+    do
+        python3 ../../machine/eval_rl.py \
+        --env $3 \
+        --model ../../machine/models/$(ls $BABY_MODEL_DIR | grep $1)/$2_check.pt \
+        --vocab ../../machine/models/$(ls $BABY_MODEL_DIR | grep $1)/vocab.json \
+        --reasoning diagnostic \
+        --diag_model $DIAG_MODEL_DIR/$4_diagseed$i.pt \
+        --episodes $EPISODES > output.log
+        ACC=$(cat output.log | grep Accuracy)
+        echo "$4 $i  | $ACC " >> eval_diag_$EPISODES.log
+    done
+
 }
 
 
 rm -f eval_diag_$EPISODES.log
 touch eval_diag_$EPISODES.log
 
-eval_diag_online 2603358 005600 BabyAI-CustomGoToObjSmall-v0 small_new_seed1_diagseed1.pt
+eval_diag_online 2603358 005600 BabyAI-CustomGoToObjSmall-v0 small_new_seed1
+eval_diag_online 2613725 006500 BabyAI-CustomGoToObjSmall-v0 small_new_seed42
+eval_diag_online 2613726 005900 BabyAI-CustomGoToObjSmall-v0 small_new_seed100
+eval_diag_online 2604666 005900 BabyAI-CustomGoToObjAndOr-v0 andor_new_seed1
+eval_diag_online 2607672 005400 BabyAI-CustomGoToObjAndOr-v0 andor_new_seed42
+eval_diag_online 2607673 006200 BabyAI-CustomGoToObjAndOr-v0 andor_new_seed100
+eval_diag_online 2604667 005400 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed1
+eval_diag_online 2607675 005600 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed42
+eval_diag_online 2607674 006500 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed100
+eval_diag_online 2300034 007600 BabyAI-CustomGoToObjSmall-v0 small_base_seed1
+eval_diag_online 2489287 007600 BabyAI-CustomGoToObjSmall-v0 small_base_seed42
+eval_diag_online 2607643 007100 BabyAI-CustomGoToObjSmall-v0 small_base_seed100
+eval_diag_online 2604660 003000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed1
+eval_diag_online 2607640 007000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed42
+eval_diag_online 2607638 007100 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed100
+eval_diag_online 2604665 002300 BabyAI-CustomGoToObjAndOr-v0 andor_base_seed1
+eval_diag_online 2607635 006900 BabyAI-CustomGoToObjAndOr-v0 andor_base_seed42
+eval_diag_online 2607636 006800 BabyAI-CustomGoToObjAndOr-v0 andor_base_seed100
