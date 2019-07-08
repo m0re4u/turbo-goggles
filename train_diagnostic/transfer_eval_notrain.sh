01ab205c06741f12cba0172aff698872c757e3d0
Michiel van der Meer <michiel@vandermeer.ai>
2019-07-08 17:23:03 +0200

    eval on transfer without training for zero-shot

---
+++ train_diagnostic/transfer_eval_notrain.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+set -eu
+
+EPISODES="${1:-100}"
+BABY_MODEL_DIR="../../machine/models/"
+
+function transfer_eval {
+    # $1 jobid
+    # $2 checkno
+    # $3 diag targets
+    # $4 LevelName
+    # $5 name
+    for i in $(seq 1 3);
+    do
+        echo "$5_$i" >> transfer_eval_$EPISODES.log
+        python3 ../../machine/eval_rl.py \
+        --env $4 \
+        --model ../../machine/models/$(ls $BABY_MODEL_DIR | grep $1)/$2_check.pt \
+        --vocab ../../machine/models/$(ls $BABY_MODEL_DIR | grep $1)/vocab.json \
+        --reasoning diagnostic \
+        --diag_targets $3 \
+        --machine \
+        --seed $i \
+        --episodes $EPISODES >> transfer_eval_$EPISODES.log;
+    done
+}
+
+
+rm -f transfer_eval_$EPISODES.log
+touch transfer_eval_$EPISODES.log
+
+transfer_eval 2300034 007600 21 BabyAI-TransferGoToObjSmall0-v0 base_small_1_color
+# transfer_eval 2489287 007600 BabyAI-TransferGoToObjSmall0-v0 base_small_42_color
+# transfer_eval 2607643 007100 BabyAI-TransferGoToObjSmall0-v0 base_small_100_color
+
+transfer_eval 2300034 007600 24 BabyAI-TransferGoToObjSmall1-v0 base_small_1_obj
+# transfer_eval 2489287 007600 BabyAI-TransferGoToObjSmall1-v0 base_small_42_obj
+# transfer_eval 2607643 007100 BabyAI-TransferGoToObjSmall1-v0 base_small_100_obj
+
+transfer_eval 2300034 007600 28 BabyAI-TransferGoToObjSmall2-v0 base_small_1_colobj
+# transfer_eval 2489287 007600 BabyAI-TransferGoToObjSmall2-v0 base_small_42_colobj
+# transfer_eval 2607643 007100 BabyAI-TransferGoToObjSmall2-v0 base_small_100_colobj
+
+
+transfer_eval 2603358 005600 21 BabyAI-TransferGoToObjSmall0-v0 new_small_1_color
+transfer_eval 2603358 005600 24 BabyAI-TransferGoToObjSmall0-v0 new_small_1_obj
+transfer_eval 2603358 005600 28 BabyAI-TransferGoToObjSmall0-v0 new_small_1_colobj
