c56dd9fcdf53ffc73d79eb7702320ba9ce392122
Michiel van der Meer <michiel@vandermeer.ai>
2019-07-22 16:29:57 +0200

    Easier aggregation for results

--- train_diagnostic/agg_transfer.py
+++ train_diagnostic/agg_transfer.py
@@ -1,59 +1,63 @@
+import os
 import argparse
 import numpy as np
 
 from itertools import product
+from collections import defaultdict
 
-
-LVLS = ['small', 'beforeafter', 'andor']
+TABLES = ['base', 'zero', 'trained_transfer']
+LVLS = ['small', 'beforeafter', 'and']
 MODELS = ['new', 'base']
 TMODE = ['color', 'obj', 'colobj']
 NSEEDS = [1, 2, 3]
 
 
-def main(args):
+def fill_results(filenames, table=None):
     result_dict = {}
-    if args.baseline:
-        for model, level in product(LVLS, MODELS):
-            mname = f"{model}_{level}"
-            result_dict[mname] = {
-                'eps': [],
-                'suc': [],
-                'fail': [],
-                'time': [],
-                'frames': []
-            }
+    if table == 'base':
+        model_generator = product(LVLS, MODELS)
+        name_idx = [0,1]
+    elif table == 'zero':
+        model_generator = product(LVLS, MODELS, TMODE)
+        name_idx = [1,0,3]
+    elif table == 'trained_transfer':
+        model_generator = product(LVLS, MODELS, TMODE)
+        name_idx = [0,1,3]
     else:
-        for model, level, tmode in product(LVLS, MODELS, TMODE):
-            mname = f"{level}_{model}_{tmode}"
-            result_dict[mname] = {
+        print("Unknown table, exiting")
+        exit(1)
+
+    # Create empty result dict
+    for m in model_generator:
+        mname = "_".join(m)
+        result_dict[mname] = {
                 'eps': [],
                 'suc': [],
                 'fail': [],
                 'time': [],
                 'frames': []
-            }
+        }
 
-    print(result_dict.keys())
-    with open(args.filename, 'r') as f:
-        for line in f:
-            spl = line.split(',')
-            name = spl[-1]
-            n_spl = name.split('_')
-            if args.baseline:
-                mkey = f"{n_spl[0]}_{n_spl[1]}"
-            elif args.trained_transfer:
-                mkey = f"{n_spl[1]}_{n_spl[2]}_{n_spl[4]}"
-            else:
-                mkey = f"{n_spl[0]}_{n_spl[1]}_{n_spl[3]}"
-            print(mkey)
-            result_dict[mkey]['frames'].append(float(spl[0].strip()))
-            result_dict[mkey]['eps'].append(float(spl[2].strip()))
-            result_dict[mkey]['suc'].append(float(spl[3].strip()))
-            result_dict[mkey]['fail'].append(float(spl[4].strip()))
-            result_dict[mkey]['time'].append(float(spl[5].strip()))
+    # Fill dict with results from file
+    for filename in filenames:
+        with open(filename, 'r') as f:
+            for line in f:
+                spl = line.split(',')
+                name = spl[-1]
+                n_spl = name.split('_')
+                mkey = "_".join([n_spl[i] for i in name_idx])
+                result_dict[mkey]['frames'].append(float(spl[0].strip()))
+                result_dict[mkey]['eps'].append(float(spl[2].strip()))
+                result_dict[mkey]['suc'].append(float(spl[3].strip()))
+                result_dict[mkey]['fail'].append(float(spl[4].strip()))
+                result_dict[mkey]['time'].append(float(spl[5].strip()))
 
-    print(result_dict)
-    for k, v in result_dict.items():
+    return result_dict
+
+def print_results(results):
+    print()
+    print("MODEL,FRAMES,EP_AVG,EP_STD,SUC_AVG,SUC_STD,FAIL_AVG,FAIL_STD,TIME_AVG,TIME_STD")
+    for k, v in results.items():
         if len(v['frames']) == 0:
             continue
         # sum frames, average + std other four metrics
@@ -62,8 +66,32 @@ def main(args):
         fail_avg, fail_std = get_stats(v['fail'])
         time_avg, time_std = get_stats(v['time'])
         print(
-            f"{k},{np.sum(v['frames'])},{ep_avg},{ep_std},{suc_avg},{suc_std},{fail_avg},{fail_std},{time_avg},{time_std}")
+            f"{k},{np.sum(v['frames']):.0f},{ep_avg:.2f},{ep_std:.2f},{suc_avg},{suc_std},{fail_avg},{fail_std},{time_avg},{time_std}")
 
+
+
+def main(args):
+    fnames = []
+    if args.table == 'base':
+        fnames = [f'lisa_create_datasets_{args.episodes}.log']
+    elif args.table == 'zero':
+        for lvlname in LVLS:
+            fname = f'transfer_eval_{args.episodes}_{lvlname}.log'
+            if not os.path.exists(fname):
+                continue
+            fnames.append(fname)
+    elif args.table == 'trained_transfer':
+        for lvlname in LVLS:
+            fname = f'trained_transfer_eval_{args.episodes}_{lvlname}.log'
+            if not os.path.exists(fname):
+                continue
+            fnames.append(fname)
+
+    # Fill the results from eval_rl
+    result_dict = fill_results(fnames, table=args.table)
+
+    # Pretty print for copying
+    print_results(result_dict)
 
 def get_stats(arr):
     return np.average(arr), np.std(arr)
@@ -71,9 +99,11 @@ def get_stats(arr):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filename", type=str, default="transfer_eval_100.log",
-                        help="")
-    parser.add_argument("--baseline", default=False, action='store_true',help="run in baseline mode")
-    parser.add_argument("--trained_transfer", default=False, action='store_true',help="trained transfer model results")
+    parser.add_argument("--table", default=None, help="Table name")
+    parser.add_argument("--episodes",type=int, default=100, help="Number of episodes that was evaluated")
     args = parser.parse_args()
-    main(args)
+    if args.table is None or args.table not in TABLES:
+        print("Specify a (correct) table name to aggregate results for!")
+        exit(1)
+    else:
+        main(args)
