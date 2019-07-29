4bee92cccf551e96783f21463be1c7e07a0163a8
Michiel van der Meer <michiel@vandermeer.ai>
2019-07-29 11:55:07 +0200

    Confusion matrix update

--- train_diagnostic/clean_dir.sh
+++ train_diagnostic/clean_dir.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 read -p "Are you sure? [y/n] " -n 1 -r
-echo    # (optional) move to a new line
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # do dangerous stuff
     rm -rf data/*
     rm -rf diags/*
+    rm -rf confusions/*
     rm -f *.log
 else
     echo "Did nothing."
--- train_diagnostic/confusion_plots.py
+++ train_diagnostic/confusion_plots.py
@@ -1,11 +1,30 @@
-import glob
-import numpy as np
+import argparse
+import os
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-for file in glob.glob('confusion*.log'):
-    plt.clf()
-    W = np.loadtxt(file)
-    plt.imshow(W)
-    plt.colorbar()
-    plt.savefig(f'{file.strip(".log")}.png')
+def main(args):
+    for file in os.listdir(args.data_dir):
+        if not file.endswith('.log'):
+            continue
+        plt.clf()
+        W = np.loadtxt(os.path.join(args.data_dir, file))
+        X_size, Y_size = W.shape
+        plt.imshow(W)
+        plt.xlabel(r'predictions $\rightarrow$')
+        plt.ylabel(r'true reason $\rightarrow$', rotation=90)
+        plt.xticks(np.arange(X_size))
+        plt.yticks(np.arange(Y_size))
+        plt.colorbar()
+        plt.savefig(f'{os.path.join(args.data_dir,file.strip(".log"))}.png')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--data_dir', default='confusions',
+                        help='directory containing confusion matrix data')
+    args = parser.parse_args()
+    main(args)
