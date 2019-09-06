9a6cbc67dfa46c185fde141058c50336aac86ed9
Michiel van der Meer <michiel@vandermeer.ai>
2019-09-06 16:10:55 +0200

    Run all eval again

--- train_diagnostic/create_datasets.sh
+++ train_diagnostic/create_datasets.sh
@@ -24,43 +24,42 @@ function create_datasets {
 rm -f create_datasets_$EPISODES.log
 touch create_datasets_$EPISODES.log
 
-# create_datasets 2654515 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed1
-# create_datasets 2669640 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed42
-# create_datasets 2669641 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed100
-# create_datasets 2651250 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed1
-# create_datasets 2654157 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed42
-# create_datasets 2669857 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed100
+create_datasets 2654515 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed1
+create_datasets 2669640 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed42
+create_datasets 2669641 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed100
+create_datasets 2651250 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed1
+create_datasets 2654157 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed42
+create_datasets 2669857 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed100
 
-# create_datasets 2709216 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed1
-# create_datasets 2709217 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed42
-# create_datasets 2709218 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed100
-# create_datasets 2691623 010000 BabyAI-CustomGoToObjAnd-v0 and_new_seed1
-# create_datasets 2731546 009500 BabyAI-CustomGoToObjAnd-v0 and_new_seed43
-# create_datasets 2691625 010000 BabyAI-CustomGoToObjAnd-v0 and_new_seed100
+create_datasets 2709216 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed1
+create_datasets 2709217 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed42
+create_datasets 2709218 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed100
+create_datasets 2691623 010000 BabyAI-CustomGoToObjAnd-v0 and_new_seed1
+create_datasets 2731546 009500 BabyAI-CustomGoToObjAnd-v0 and_new_seed43
+create_datasets 2691625 010000 BabyAI-CustomGoToObjAnd-v0 and_new_seed100
 
-# create_datasets 2669860 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed1
-# create_datasets 2669861 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed42
-# create_datasets 2669862 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed100
-# create_datasets 2689034 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed1
-# create_datasets 2689035 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed42
-# create_datasets 2689036 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed100
-
+create_datasets 2669860 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed1
+create_datasets 2669861 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed42
+create_datasets 2669862 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed100
+create_datasets 2689034 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed1
+create_datasets 2689035 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed42
+create_datasets 2689036 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed100
 
 create_datasets 2782514 022000 BabyAI-CustomGoToObjSmall-v0 sparsesmall_new_seed1
 create_datasets 2782515 022000 BabyAI-CustomGoToObjSmall-v0 sparsesmall_new_seed42
 create_datasets 2827355 032000 BabyAI-CustomGoToObjMedium-v0 sparsebeforeafter_new_seed1
 create_datasets 2827356 032000 BabyAI-CustomGoToObjMedium-v0 sparsebeforeafter_new_seed42
 
-# create_datasets 2818253 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed1
-# create_datasets 2818254 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed42
-# create_datasets 2818255 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed100
-# create_datasets 2812454 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed1
-# create_datasets 2812455 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed42
-# create_datasets 2812456 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed100
+create_datasets 2818253 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed1
+create_datasets 2818254 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed42
+create_datasets 2818255 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed100
+create_datasets 2812454 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed1
+create_datasets 2812455 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed42
+create_datasets 2812456 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed100
 
-# create_datasets 2818257 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed1
-# create_datasets 2818258 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed42
-# create_datasets 2818260 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed100
-# create_datasets 2812451 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed1
-# create_datasets 2812453 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed42
-# create_datasets 2812452 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed100
+create_datasets 2818257 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed1
+create_datasets 2818258 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed42
+create_datasets 2818260 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed100
+create_datasets 2812451 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed1
+create_datasets 2812453 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed42
+create_datasets 2812452 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed100
--- train_diagnostic/eval_diags.sh
+++ train_diagnostic/eval_diags.sh
@@ -29,42 +29,42 @@ function eval_diags_online {
 rm -f eval_diags_$EPISODES.log
 touch eval_diags_$EPISODES.log
 
-# eval_diags_online 2654515 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed1
-# eval_diags_online 2669640 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed42
-# eval_diags_online 2669641 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed100
-# eval_diags_online 2651250 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed1
-# eval_diags_online 2654157 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed42
-# eval_diags_online 2669857 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed100
+eval_diags_online 2654515 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed1
+eval_diags_online 2669640 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed42
+eval_diags_online 2669641 010000 BabyAI-CustomGoToObjSmall-v0 small_base_seed100
+eval_diags_online 2651250 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed1
+eval_diags_online 2654157 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed42
+eval_diags_online 2669857 010800 BabyAI-CustomGoToObjSmall-v0 small_new_seed100
 
-# eval_diags_online 2709216 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed1
-# eval_diags_online 2709217 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed42
-# eval_diags_online 2709218 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed100
-# eval_diags_online 2691623 010000 BabyAI-CustomGoToObjAnd-v0 and_new_seed1
-# eval_diags_online 2731546 009500 BabyAI-CustomGoToObjAnd-v0 and_new_seed43
-# eval_diags_online 2691625 010000 BabyAI-CustomGoToObjAnd-v0 and_new_seed100
+eval_diags_online 2709216 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed1
+eval_diags_online 2709217 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed42
+eval_diags_online 2709218 010000 BabyAI-CustomGoToObjAnd-v0 and_base_seed100
+eval_diags_online 2691623 010000 BabyAI-CustomGoToObjAnd-v0 and_new_seed1
+eval_diags_online 2731546 009500 BabyAI-CustomGoToObjAnd-v0 and_new_seed43
+eval_diags_online 2691625 010000 BabyAI-CustomGoToObjAnd-v0 and_new_seed100
 
-# eval_diags_online 2669860 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed1
-# eval_diags_online 2669861 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed42
-# eval_diags_online 2669862 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed100
-# eval_diags_online 2689034 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed1
-# eval_diags_online 2689035 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed42
-# eval_diags_online 2689036 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed100
+eval_diags_online 2669860 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed1
+eval_diags_online 2669861 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed42
+eval_diags_online 2669862 014000 BabyAI-CustomGoToObjMedium-v0 beforeafter_base_seed100
+eval_diags_online 2689034 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed1
+eval_diags_online 2689035 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed42
+eval_diags_online 2689036 025000 BabyAI-CustomGoToObjMedium-v0 beforeafter_new_seed100
 
 eval_diags_online 2782514 022000 BabyAI-CustomGoToObjSmall-v0 sparsesmall_new_seed1
 eval_diags_online 2782515 022000 BabyAI-CustomGoToObjSmall-v0 sparsesmall_new_seed42
 eval_diags_online 2827355 032000 BabyAI-CustomGoToObjMedium-v0 sparsebeforeafter_new_seed1
 eval_diags_online 2827356 032000 BabyAI-CustomGoToObjMedium-v0 sparsebeforeafter_new_seed42
 
-# eval_diags_online 2818253 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed1
-# eval_diags_online 2818254 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed42
-# eval_diags_online 2818255 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed100
-# eval_diags_online 2812454 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed1
-# eval_diags_online 2812455 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed42
-# eval_diags_online 2812456 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed100
+eval_diags_online 2818253 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed1
+eval_diags_online 2818254 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed42
+eval_diags_online 2818255 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_base_seed100
+eval_diags_online 2812454 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed1
+eval_diags_online 2812455 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed42
+eval_diags_online 2812456 021900 BabyAI-CustomGoToObjMultiple-v0 multiple_new_seed100
 
-# eval_diags_online 2818257 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed1
-# eval_diags_online 2818258 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed42
-# eval_diags_online 2818260 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed100
-# eval_diags_online 2812451 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed1
-# eval_diags_online 2812453 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed42
-# eval_diags_online 2812452 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed100
+eval_diags_online 2818257 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed1
+eval_diags_online 2818258 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed42
+eval_diags_online 2818260 024900 BabyAI-CustomGoToObjThrees-v0 threes_base_seed100
+eval_diags_online 2812451 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed1
+eval_diags_online 2812453 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed42
+eval_diags_online 2812452 024900 BabyAI-CustomGoToObjThrees-v0 threes_new_seed100
